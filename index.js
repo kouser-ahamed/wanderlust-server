a94a8499dbe2b56b146dce5b4737b1bafd02ec1b
kouser-ahamed <ahamed15-5643@diu.edu.bd>
2026-05-16 19:16:24 +0600

    Implement PATCH and DELETE endpoints for destination routes

--- index.js
+++ index.js
@@ -45,9 +45,21 @@ async function run() {
       res.json(result);
     })
 
+    app.patch("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const updateData = req.body;
+      const result = await destinationsCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updateData }
+      );
+      res.json(result);
+    })
 
-
-
+    app.delete("/destination/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await destinationsCollection.deleteOne({ _id: new ObjectId(id) });
+      res.json(result);
+    })
 
 
 
